2a402aaca9179a8fe890e7ea82f27398a39c5f96
Derly Pacheco <derly.pacheco@outlook.com>
2021-01-28 17:23:51 -0700

    navbar update

--- dashboard.php
+++ dashboard.php
@@ -54,27 +54,39 @@
     <!-- Page Content  -->
     <div id="content">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-principal">
             <div class="container-fluid">
                 <button type="button" id="sidebarCollapse" class="btn btn-default">
-                    <i class="fas fa-align-left"></i>
+                    <i class="fas text-white fa-align-left"></i>
                 </button>
+                <div class="dropdown">
+                    <a href="" class="" data-toggle="dropdown" aria-expanded="false">
+                        <img src="src/img/SPADESKTOP-01.png" class="thumbnail rounded-circle" width="40">
+                        <span class="my-1">Derly Ociel Pacheco Fabela</span>
+                        <i class="fas fa-caret-down"></i>
+                    </a>
+                        <ul class="dropdown-menu p-3 dropdown-right bg-complement">
+                            <li class="p-2"><a href="#"> <i class="pe-2 fa fa-user"></i> Mis datos</a></li>
+                            <li class="p-2"><a href="#"> <i class="pe-2 fa fa-cog"></i>Preferencias</a></li>
+                            <hr>
+                            <li class="p-2"><a href="#"> <i class="pe-2 fa fa-sign-out-alt"></i> Salir</a></li>
+                        </ul>
+                </div>
             </div>
         </nav>
 
-
-            <div class="form-group my-3">
-                <div class="row">
-                    <div class="col-8">
-                        <h2 class="text-white align-content-sm-center">
-                            Listado de usuarios
-                        </h2>
-                    </div>
-                    <div class="col">
-                        <input name="" class="form-control" placeholder="Buscar usuario">
-                    </div>
+        <div class="form-group my-3">
+            <div class="row">
+                <div class="col-8">
+                    <h2 class="text-white align-content-sm-center">
+                        Listado de usuarios
+                    </h2>
+                </div>
+                <div class="col">
+                    <input name="" class="form-control" placeholder="Buscar usuario">
                 </div>
             </div>
+        </div>
 
         <div class="form-group my-3">
             <div class="row">
